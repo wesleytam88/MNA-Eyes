80a39a563e416abf3f6ec3eda7b40cb808ca4097
Wesley-Tam <21tamw24@stu.smuhsd.org>
2023-01-23 19:45:38 -0800

    Add comments

--- Button.py
+++ Button.py
@@ -16,17 +16,23 @@ class Button():
         self.text_rect = self.text.get_rect(center = (self.x_pos, self.y_pos))
         
     def update(self, screen):
+        '''Displays the text onto the screen'''
+
         if self.image is not None:
             screen.blit(self.image, self.rect)
         screen.blit(self.text, self.text_rect)
 
     def checkForInput(self, mouse_pos):
+        '''Returns true/false if button was pressed'''
+
         if (mouse_pos[0] in range(self.rect.left, self.rect.right) and 
                 mouse_pos[1] in range(self.rect.top, self.rect.bottom)):
             return True
         return False
 
     def changeColor(self, mouse_pos):
+        '''Code for changing the button color when hovering over the button'''
+
         if (mouse_pos[0] in range(self.rect.left, self.rect.right) and 
                 mouse_pos[1] in range(self.rect.top, self.rect.bottom)):
             self.text = self.font.render(self.text_input, True, self.hover_color)
